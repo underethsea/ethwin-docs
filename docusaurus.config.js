f9d9895aa4db9113890a16b989af8e39618202cf
underethsea <underethsea@gmail.com>
2022-11-07 22:29:45 -0800

    build ok w broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'WIN WIN',
   url: 'https://ethwindocs.netlify.app/',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
